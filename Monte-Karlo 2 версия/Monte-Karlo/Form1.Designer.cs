ed73c8be6199ee0b46f9d335dd0b61b684c6f339
tradxxx <107495941+tradxxx@users.noreply.github.com>
2022-06-14 19:41:57 +0300

    Monte-Karlo
    Monte-Karlo

---
+++ Monte-Karlo 2 версия/Monte-Karlo/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Monte_Karlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(647, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нарисовать круг и квадрат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(647, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Бросить 10 000 точек";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(647, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Monte-Karlo 2 версия/Monte-Karlo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monte_Karlo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            g.DrawRectangle(new Pen(Color.Black,3),50,50,300,300);
+            g.DrawEllipse(new Pen(Color.Blue,3),50,50,300,300);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int insidCount = 0;
+            int outsideCount = 0;
+            Graphics g2 = CreateGraphics();
+            Random r = new Random();
+            Pen p1;
+            for (int i = 0; i < 10000; i++)
+            {
+                int rx = r.Next(50, 351);
+                int ry = r.Next(50, 351);
+
+                //Центр окружности в точке (200,200)
+
+               
+
+                if (Math.Pow(rx-200,2)+Math.Pow(ry-200,2)<=150*150)
+                {
+                    p1 = new Pen(Color.Orange);
+                    insidCount++;
+                }
+                else
+                {
+                    p1 = new Pen(Color.Blue);
+                }
+                g2.DrawRectangle(p1, rx, ry, 1, 1);
+
+                
+            }
+            double S = (double)90000 * insidCount / 10000;
+            double Pi = (double)Math.Pow(300,2)/Math.Pow(150,2)*insidCount/10000;
+            richTextBox1.Text += "S круга = " + S+"\n"+"Pi = "+Pi;
+        }
+
+       
+    }
+}
